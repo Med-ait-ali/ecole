500b4c17cbce6b36c6b22cd4879576c7e2d7dada
Med-ait-ali <ait.france59@gmail.com>
2026-03-06 02:29:13 +0000

    Fix: wrap App with SchoolProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { SchoolProvider } from './context/SchoolContext'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>  {/* من دون basename */}
-      <App />
+    <BrowserRouter>
+      <SchoolProvider>
+        <App />
+      </SchoolProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
